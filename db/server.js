afc60c52100a93ff974f2815cd661bd49ef71ac6
JordanPC <Galacticvoice@gmail.com>
2021-03-06 21:13:13 -0500

    Commit for Heroku Deployment!

--- db/server.js
+++ db/server.js
@@ -22,9 +22,10 @@ app.get('/api/notes',(req,res)=>{fs.readFile('/db.json', (err,dat) => {res.send(
 app.get('*',(req,res)=>{res.sendFile(path.join(__dirname, '../public/index.html'));});
 
 app.post('/api/notes', (req,res) => {
-    const userNote = req.body;
+    const newNote = req.body;
     fs.readFile('/db.json', (err,dat)=>{
         const noteStore = JSON.parse(dat);
-        noteStore.push(userNote);
+        noteStore.push(newNote);
     });
 });
+
